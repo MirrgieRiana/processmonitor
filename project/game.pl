40cf48bd29131c9ba44fc2b002d09643253e29ef
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-09-20 21:04:27 +0900

    add: game.pl

---
+++ project/game.pl
@@ -0,0 +1,41 @@
+
+open $log, ">", "latest.log";
+
+sub out( $ ) {
+	print "$_[0]\n";
+	print $log "STDOUT: $_[0]\n";
+}
+sub err( $ ) {
+	print STDERR "$_[0]\n";
+	print $log "STDERR: $_[0]\n";
+}
+
+
+$SIG{INT} = sub {
+	err "INT!!!!!";
+	exit;
+};
+$SIG{HUP} = sub {
+	err "HUP!!!!!";
+	exit;
+};
+$SIG{PIPE} = sub {
+	err "PIPE!!!!!";
+	exit;
+};
+$SIG{TERM} = sub {
+	err "TERM!!!!!";
+	exit;
+};
+
+while ($a = <>) {
+	chomp $a;
+	if ($a eq "stop") {
+		out "STOP!";
+		exit;
+	}
+	out "{$a}";
+	err "Error!" if rand() < 0.2;
+}
+
+out "EOF!";
