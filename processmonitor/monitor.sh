67aaaa1d55debf58a9638669340a82c1636a33f5
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-09-24 15:02:34 +0900

    add: monitor.sh

---
+++ processmonitor/monitor.sh
@@ -0,0 +1,28 @@
+cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+eval $(cat config.sh)
+
+# stdin -> pipe1
+echo <(
+
+	while true
+	do
+		if read -r line; then
+			echo $line > $pipe1_file
+		else
+			break
+		fi
+	done
+
+	echo "$0: Stdin closed!" >&2
+
+) > /dev/null
+
+# run monitor
+(cd "$project_dir"; tail -F "$log_file")
+
+# terminate
+for pid in $(bash processtree.sh $$)
+do
+	kill $pid
+	wait $pid 2> /dev/null
+done
