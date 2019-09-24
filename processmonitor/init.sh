636aaa1e602b8f583a730ff1a25e9bc89c8ce0a9
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-09-24 10:10:26 +0900

    init.sh: lockファイルが標準ファイルでなければエラーになるように

--- processmonitor/init.sh
+++ processmonitor/init.sh
@@ -15,7 +15,12 @@ fi
 
 # make lock if do not exist
 if [ ! -f $lock_file ]; then
-	if [ ! $(touch $lock_file; echo $?) -eq "0" ]; then
+	if [ -e $lock_file ]; then
+		echo "$0: $lock_file: Is not a file" >&2
 		exit 1
+	else
+		if [ ! $(echo "" > $lock_file; echo $?) -eq "0" ]; then
+			exit 1
+		fi
 	fi
 fi
