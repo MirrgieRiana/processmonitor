08aa027412d4ec6eb1adf5402522a06849d6e26e
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-09-25 09:13:47 +0900

    monitor, send: initするように

--- processmonitor/monitor.sh
+++ processmonitor/monitor.sh
@@ -1,6 +1,9 @@
 cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 eval $(cat config.sh)
 
+# init
+bash init.sh || exit 1
+
 # stdin -> pipe1
 echo <(
 
--- processmonitor/send.sh
+++ processmonitor/send.sh
@@ -1,4 +1,7 @@
 cd $(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 eval $(cat config.sh)
 
+# init
+bash init.sh || exit 1
+
 echo "$*" > $pipe1_file
